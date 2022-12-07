eb624c4099aac00e508360a9ba05bb79b8741249
natecdr <nathan.candre@gmail.com>
2022-12-07 12:36:54 +0100

    working filter

--- pynth/filter.py
+++ pynth/filter.py
@@ -1,8 +1,25 @@
 import numpy as np
 
 class Filter:
-    def __init__(self, type = "lowpass", cutoff_frequency = np.inf):
+    def __init__(self, type = "lowpass", cutoff_frequency = 2000):
         self.type = type
         self.cutoff_frequency = cutoff_frequency
+        
+        self.dn_1 = 0 #Inner buffer of the filter
     
-    
+    def apply_filter(self, signal, sample_rate):
+        filter_output = np.zeros_like(signal)
+        break_frequency = self.cutoff_frequency
+        tan = np.tan(np.pi * break_frequency / sample_rate)
+        a1 = (tan - 1) / (tan + 1)
+        
+        for i in range(len(signal)):
+            filter_output[i] = a1 * signal[i] + self.dn_1
+        
+            self.dn_1 = signal[i] - a1 * filter_output[i]
+        
+        if self.type == "highpass":
+            filter_output *= -1
+            
+        return (signal + filter_output) * 0.5
+        
--- pynth/oscillator.py
+++ pynth/oscillator.py
@@ -2,16 +2,18 @@ import numpy as np
 import pynth.waveforms as waveforms
 from pynth.wavetable import Wavetable
 from pynth.envelope import Envelope
+from pynth.filter import Filter
 from pynth.utils import *
 
 import matplotlib.pyplot as plt
 
 class Oscillator:
-    def __init__(self, synth, wavetable = Wavetable(waveforms.sine), envelope = Envelope(), gain = -10):
+    def __init__(self, synth):
         self.synth = synth
-        self.wavetable = wavetable
-        self.envelope = envelope
-        self.gain = gain
+        self.wavetable = Wavetable(waveforms.sine)
+        self.envelope = Envelope()
+        self.filter = Filter("highpass")
+        self.gain = -10
         
     def output_signal(self, f, time, sample_rate):
         signal = self.build_signal(f, time, sample_rate)
@@ -24,18 +26,24 @@ class Oscillator:
     
     def build_signal(self, f, time, sample_rate, release = False):
         index_increment = f * self.wavetable.n_samples / sample_rate
-        
+
         signal = np.zeros(int(time * sample_rate))
         for isignal in range(0, len(signal), self.synth.BUFFER_SIZE):
-            buffer = np.zeros(min(len(signal) - isignal, self.synth.BUFFER_SIZE))
-            for ibuffer in range(len(buffer)):
-                buffer[ibuffer] = interpolate_linearly(self.wavetable, self.wavetable.index)
-                buffer[ibuffer] = self.envelope.apply_envelope(buffer[ibuffer], isignal + ibuffer, sample_rate, release=release)
-                
-                self.wavetable.index = (self.wavetable.index + index_increment) % self.wavetable.n_samples
+            buffer = self.build_buffer(signal, isignal, sample_rate, index_increment, release)
             signal[isignal:isignal+self.synth.BUFFER_SIZE] = buffer
             
         return signal
+    
+    def build_buffer(self, signal, isignal, sample_rate, index_increment, release):
+        buffer = np.zeros(min(len(signal) - isignal, self.synth.BUFFER_SIZE))
+        for ibuffer in range(len(buffer)):
+            buffer[ibuffer] = interpolate_linearly(self.wavetable, self.wavetable.index)
+            buffer[ibuffer] = self.envelope.apply_envelope(buffer[ibuffer], isignal + ibuffer, sample_rate, release=release)
+            
+            self.wavetable.index = (self.wavetable.index + index_increment) % self.wavetable.n_samples
+        
+        buffer = self.filter.apply_filter(buffer, sample_rate)
+        return buffer
     
     def apply_gain(self, signal):
         amplitude = 10 ** (self.gain/20)
