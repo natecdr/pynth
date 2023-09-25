b305cf1a2b1cecd284979c0c73993fb406cc95da
natecdr <nathan.candre@gmail.com>
2023-09-25 13:01:03 +0200

    Add filter cutoff

--- pynth/controls.py
+++ pynth/controls.py
@@ -2,7 +2,14 @@ from tkinter import *
 
 class Slider(Scale):
     def __init__(self, *args, synth_parameter, **kwargs):
-        super().__init__(*args, **kwargs, from_ = max(synth_parameter.range), to=min(synth_parameter.range), command=self.onChange)
+        if kwargs.get("orient") == VERTICAL:
+            range_bottom = max(synth_parameter.range)
+            range_top = min(synth_parameter.range)
+        else:
+            range_bottom = min(synth_parameter.range)
+            range_top = max(synth_parameter.range)
+            
+        super().__init__(*args, **kwargs, from_ = range_bottom, to=range_top, command=self.onChange)
         self.synth_parameter = synth_parameter
         self.set(self.synth_parameter.value)
         
